f43082e631bd9f4929990be52636e1339d8f55cd
Majkl <majkl.andracic@gmail.com>
2020-04-06 22:43:38 +0200

    Connecting to Firebase

---
+++ app/src/main/java/com/majkl/kiteboardingcroatia/MainActivity.java
@@ -0,0 +1,40 @@
+package com.majkl.kiteboardingcroatia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        printKeyHash;
+    }
+
+    private void printKeyHash(){
+        try{
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for(Signature signature : packageInfo.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEYHASH", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+            
+        }catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
