ab0cfa420f2ddbb75d948ef0af04755a4ada5b8b
Majkl <majkl.andracic@gmail.com>
2020-04-10 21:53:54 +0200

    Added bottom menu with fragments layout

---
+++ app/src/main/java/com/majkl/kiteboardingcroatia/Model/User.java
@@ -0,0 +1,15 @@
+package com.majkl.kiteboardingcroatia.Model;
+
+public class User {
+    private String name,address,phoneNumber;
+
+    public User(){
+
+    }
+
+    public User(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
